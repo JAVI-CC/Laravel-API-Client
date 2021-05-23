3110dc819aca19a7bee721c5d2a89b585921c1e3
JAVI-CC <javileg98@hotmail.com>
2021-05-23 19:38:16 +0200

    style not_found page

--- resources/views/not_found.blade.php
+++ resources/views/not_found.blade.php
@@ -21,15 +21,16 @@
 
 @endsection('search')
 
-<div class="div-not-found col-md-12">
+<div class="div-not-found col-md-12" style="padding-left: 0px; padding-right: 0px;">
+
   <div class="div-text-not-found">
-    <span class="text-not-found">{{ $error }}</span>
-    <div></div>
+  <span class="text-not-found">{{ $error }}</span>
+  <div style="margin-top: 10px;"></div>
+  <img src="/offline.png" alt="offline" width="220px" >
+    <div style="margin-top: 10px;"></div>
     <a href="/" class="link-not-found">VOLVER AL INICIO</a>
   </div>
-  <i class="fas fa-exclamation icon-exclamation"></i>
-  <i class="fas fa-exclamation icon-exclamation"></i>
-  <i class="fas fa-exclamation icon-exclamation"></i>
+
 </div>
 
 
