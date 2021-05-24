2beb797484397118dec8ed22e2a341306a4b49da
JAVI-CC <javileg98@hotmail.com>
2021-05-24 11:58:01 +0200

    update laravel 8 and php 8

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JuegoController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,35 +15,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'JuegoController@index')->name('juegos.index');
+Route::get('/', [JuegoController::class, 'index'])->name('juegos.index');
 
 Route::prefix('auth/')->group(function () {
     //Route::post('register', 'AuthController@register')->name('register');
-    Route::post('login', 'AuthController@login')->name('login');
-    Route::post('logout', 'AuthController@logout')->name('logout');
-    Route::get('userinfo', 'AuthController@userinfo')->name('userinfo');
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('userinfo', [AuthController::class, 'userinfo'])->name('userinfo');
 });
 
 Route::prefix('juegos/')->group(function () {
 
-    Route::get('add', 'JuegoController@add_form')->name('add_form');
+    Route::get('add', [JuegoController::class, 'add_form'])->name('add_form');
 
-    Route::post('add', 'JuegoController@add')->name('add');
+    Route::post('add', [JuegoController::class, 'add'])->name('add');
 
-    Route::get('{slug}', 'JuegoController@show')->name('show');
+    Route::get('{slug}', [JuegoController::class, 'show'])->name('show');
 
-    Route::post('edit', 'JuegoController@update')->name('update');
+    Route::post('edit', [JuegoController::class, 'update'])->name('update');
 
-    Route::get('delete/{slug}', 'JuegoController@delete')->name('delete');
+    Route::get('delete/{slug}', [JuegoController::class, 'delete'])->name('delete');
 
-    Route::post('search', 'JuegoController@search')->name('search');
+    Route::post('search', [JuegoController::class, 'search'])->name('search');
 });
 
 /* Desarrolladora */
-Route::get('desarrolladora/{slug}', 'JuegoController@showdesarrolladora')->name('showdesarrolladora');
+Route::get('desarrolladora/{slug}', [JuegoController::class, 'showdesarrolladora'])->name('showdesarrolladora');
 
 /* Genero */
-Route::get('genero/{slug}', 'JuegoController@showgenero')->name('showgenero');
+Route::get('genero/{slug}', [JuegoController::class, 'showgenero'])->name('showgenero');
 
 /* Auth */
 Auth::routes();
